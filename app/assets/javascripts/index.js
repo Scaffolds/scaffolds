fa31d0636392152925eb7c6bc852da9602b63148
Daniel Chinedu <chinedudaniel7@gmail.com>
2016-04-10 23:51:51 +0100

    feat (UI Design) Implement basic slider (#2)
    Implement basic slider:
    - Create page specific javascript file to handle image slider
    - Include basic styling for slider

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,60 @@
+(function () {
+  /*  Prevent this script from running on other pages except the root index  */
+  if (location.pathname !== "/") { return false; }
+
+  /*
+    ==========================
+    Simple Slider v 0.0
+    ==========================
+  */
+  var simpleSlide = {};
+
+  simpleSlide.view = {
+    imgContainer: $(".image-cluster"),
+    allImages: function () {
+      return this.imgContainer.children();
+    },
+    maxLength: function () {
+      return this.allImages().length;
+    },
+    imgDimension: function () {
+      return this.allImages()[0].width;
+    }
+  };
+
+  simpleSlide.controller = {
+    functionalSlide: function () {
+      var ss = simpleSlide,
+          next = ss.view.next,
+          moveBy = 0,
+          index = 1;
+
+      setInterval(function () {
+        moveBy += ss.view.imgDimension();
+        index += 1;
+
+        if (index > (ss.view.maxLength() - 2)) {
+          moveBy = 0;
+          index = 1;
+        }
+
+        $.each(ss.view.allImages(), function (index, item) {
+          $(item).removeClass("magnify");
+        });
+
+        ss.view.imgContainer.css({
+          marginLeft: `-${moveBy}px`
+        });
+
+        ss.view.allImages().eq(index).addClass("magnify");
+      }, 5000);
+    }
+  }
+
+  simpleSlide.initialize = function () {
+    this.controller.functionalSlide();
+  }
+
+  simpleSlide.initialize();
+
+}());
