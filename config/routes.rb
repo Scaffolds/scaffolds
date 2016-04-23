7dd21d87c7e71556f8ec65719ab9eb372059cc72
Daniel Chinedu <chinedudaniel7@gmail.com>
2016-04-23 21:49:11 +0100

    Feat homepage layout (#3)
    * Add structure for featured section
    
    * Add featured and trending section
    
    * Complete desktop layout for landing page
    
    * feat (UI Design): Make homepage responsive
    
    Make homepage responsive:
    - Add sidebar popup to tablet/mobile view
    - Replace nav links with a single menu link on tablet / mobile view
    - Hide slider on extremely large screens (4k screens)
    - Make page columns stackable

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root "pages#index"
-  get "admin" => "pages#example"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
